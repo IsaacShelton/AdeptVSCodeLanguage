dc7331fdb2b59492a3828495035a834a1052bef0
IsaacShelton <isaac.a.shelton@gmail.com>
2021-06-30 10:16:02 -0500

    Converted tabs to spaces

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -7,10 +7,10 @@ import * as path from 'path';
 import { workspace, ExtensionContext, window, ConfigurationTarget } from 'vscode';
 
 import {
-	LanguageClient,
-	LanguageClientOptions,
-	ServerOptions,
-	TransportKind
+    LanguageClient,
+    LanguageClientOptions,
+    ServerOptions,
+    TransportKind
 } from 'vscode-languageclient';
 
 import * as child_process from 'child_process';
@@ -18,73 +18,73 @@ import * as child_process from 'child_process';
 let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
-	// The server is implemented in node
-	let serverModule = context.asAbsolutePath(
-		path.join('server', 'out', 'server.js')
-	);
-	
-	// The debug options for the server
-	// --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-	let debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
+    // The server is implemented in node
+    let serverModule = context.asAbsolutePath(
+        path.join('server', 'out', 'server.js')
+    );
+    
+    // The debug options for the server
+    // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
+    let debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
 
-	// If the extension is launched in debug mode then the debug server options are used
-	// Otherwise the run options are used
-	let serverOptions: ServerOptions = {
-		run: { module: serverModule, transport: TransportKind.ipc },
-		debug: {
-			module: serverModule,
-			transport: TransportKind.ipc,
-			options: debugOptions
-		}
-	};
+    // If the extension is launched in debug mode then the debug server options are used
+    // Otherwise the run options are used
+    let serverOptions: ServerOptions = {
+        run: { module: serverModule, transport: TransportKind.ipc },
+        debug: {
+            module: serverModule,
+            transport: TransportKind.ipc,
+            options: debugOptions
+        }
+    };
 
-	// Options to control the language client
-	let clientOptions: LanguageClientOptions = {
-		// Register the server for plain text documents
-		documentSelector: [{ scheme: 'file', language: 'adept' }],
-		synchronize: {
-			// Notify the server about file changes to '.clientrc files contained in the workspace
-			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
-		}
-	};
+    // Options to control the language client
+    let clientOptions: LanguageClientOptions = {
+        // Register the server for plain text documents
+        documentSelector: [{ scheme: 'file', language: 'adept' }],
+        synchronize: {
+            // Notify the server about file changes to '.clientrc files contained in the workspace
+            fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
+        }
+    };
 
-	// Create the language client and start the client.
-	client = new LanguageClient(
-		'adeptLanguageServer-v2.5',
-		'Adept Language 2.5',
-		serverOptions,
-		clientOptions
-	);
+    // Create the language client and start the client.
+    client = new LanguageClient(
+        'adeptLanguageServer-v2.5',
+        'Adept Language 2.5',
+        serverOptions,
+        clientOptions
+    );
 
-	client.onReady().then(() => {
-		client.onNotification("adeptLanguageInsight/noRoot", () => {
-			let yes: string = "Auto Configure (recommended)";
+    client.onReady().then(() => {
+        client.onNotification("adeptLanguageInsight/noRoot", () => {
+            let yes: string = "Auto Configure (recommended)";
 
-			window
-				.showInformationMessage("No Adept Root Configured! Would you like to automatically configure it?", yes, "No")
-				.then(selection => {
-					if(selection != yes){
-						window.showInformationMessage("In order to manually configure, you must set 'adeptLanguageInsight.root' to be the root folder of your desired compiler. You can easily get the root folder of an Adept compiler by running `desired-compiler --root`");
-						return;
-					}
+            window
+                .showInformationMessage("No Adept Root Configured! Would you like to automatically configure it?", yes, "No")
+                .then(selection => {
+                    if(selection != yes){
+                        window.showInformationMessage("In order to manually configure, you must set 'adeptLanguageInsight.root' to be the root folder of your desired compiler. You can easily get the root folder of an Adept compiler by running `desired-compiler --root`");
+                        return;
+                    }
 
-					child_process.exec("adept --root", (err, stdout, stderr) => {
-						stdout = stdout.trim();
-						workspace.getConfiguration()
-							.update('adeptLanguageInsight.root', stdout, ConfigurationTarget.Global);
-						window.showInformationMessage("Automatically configuration successful!");
-					});
-				});
-		});
-	});
+                    child_process.exec("adept --root", (err, stdout, stderr) => {
+                        stdout = stdout.trim();
+                        workspace.getConfiguration()
+                            .update('adeptLanguageInsight.root', stdout, ConfigurationTarget.Global);
+                        window.showInformationMessage("Automatically configuration successful!");
+                    });
+                });
+        });
+    });
 
-	// Start the client. This will also launch the server
-	context.subscriptions.push(client.start());
+    // Start the client. This will also launch the server
+    context.subscriptions.push(client.start());
 }
 
 export function deactivate(): Thenable<void> | undefined {
-	if (!client) {
-		return undefined;
-	}
-	return client.stop();
+    if (!client) {
+        return undefined;
+    }
+    return client.stop();
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -3,31 +3,31 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
 import {
-	createConnection,
-	TextDocuments,
-	Diagnostic,
-	DiagnosticSeverity,
-	ProposedFeatures,
-	InitializeParams,
-	DidChangeConfigurationNotification,
-	CompletionItem,
-	CompletionItemKind,
-	TextDocumentPositionParams,
-	TextDocumentSyncKind,
-	InitializeResult,
-	HoverParams,
-	Hover,
-	MarkupContent
+    createConnection,
+    TextDocuments,
+    Diagnostic,
+    DiagnosticSeverity,
+    ProposedFeatures,
+    InitializeParams,
+    DidChangeConfigurationNotification,
+    CompletionItem,
+    CompletionItemKind,
+    TextDocumentPositionParams,
+    TextDocumentSyncKind,
+    InitializeResult,
+    HoverParams,
+    Hover,
+    MarkupContent
 } from 'vscode-languageserver';
 
 import {
-	Range,
-	TextDocument
+    Range,
+    TextDocument
 } from 'vscode-languageserver-textdocument';
 
 interface IdentifierToken {
-	range: Range,
-	content: string;
+    range: Range,
+    content: string;
 }
 
 var insight_server = require('./insight_server.js');
@@ -36,57 +36,57 @@ var ast: any = null;
 var identifierTokens: IdentifierToken[] = [];
 
 class CompletionDetails {
-	detail: string;
-	documentation: string;
+    detail: string;
+    documentation: string;
 
-	constructor(details: string, documentation: string){
-		this.detail = details;
-		this.documentation = documentation;
-	}
+    constructor(details: string, documentation: string){
+        this.detail = details;
+        this.documentation = documentation;
+    }
 }
 
 class AutoCompletions {
-	completionItems: CompletionItem[];
-	completionItemDetails: CompletionDetails[];
+    completionItems: CompletionItem[];
+    completionItemDetails: CompletionDetails[];
 
-	constructor(){
-		this.completionItems = [];
-		this.completionItemDetails = [];
-	}
+    constructor(){
+        this.completionItems = [];
+        this.completionItemDetails = [];
+    }
 
-	// NOTE: 'item.data' is autofilled
-	add(item: CompletionItem, detail: CompletionDetails) {
-		item.data = this.completionItems.length;
+    // NOTE: 'item.data' is autofilled
+    add(item: CompletionItem, detail: CompletionDetails) {
+        item.data = this.completionItems.length;
 
-		this.completionItems.push(item);
-		this.completionItemDetails.push(detail);
-	}
+        this.completionItems.push(item);
+        this.completionItemDetails.push(detail);
+    }
 
-	getDetailedCompletionItem(item: CompletionItem) {
-		// 'item.data' is used as index for completion item
-		Object.assign(item, this.completionItemDetails[item.data]);
-		return item;
-	}
+    getDetailedCompletionItem(item: CompletionItem) {
+        // 'item.data' is used as index for completion item
+        Object.assign(item, this.completionItemDetails[item.data]);
+        return item;
+    }
 }
 
 var autoCompletion: AutoCompletions = new AutoCompletions();
 
 insight_server.Module['onRuntimeInitialized'] = function() {
-	is_wasm_initialized = true;
-	if (!insight_server.Module["noFSInit"] && !insight_server.FS.init.initialized) insight_server.FS.init();
-	insight_server.TTY.init();
-	insight_server.preMain();
+    is_wasm_initialized = true;
+    if (!insight_server.Module["noFSInit"] && !insight_server.FS.init.initialized) insight_server.FS.init();
+    insight_server.TTY.init();
+    insight_server.preMain();
 
-	documents.all().forEach(validateTextDocument);
+    documents.all().forEach(validateTextDocument);
 };
 
 function invokeInsight(query_json_string: string | object): null | string | object {
-	if(!is_wasm_initialized) return null;
-	
-	if(typeof query_json_string != "string"){
-		query_json_string = JSON.stringify(query_json_string);
-	}
-	
+    if(!is_wasm_initialized) return null;
+    
+    if(typeof query_json_string != "string"){
+        query_json_string = JSON.stringify(query_json_string);
+    }
+    
     var bytes = insight_server.lengthBytesUTF8(query_json_string);
     var cstring = insight_server._malloc(bytes + 1);
     insight_server.stringToUTF8(query_json_string, cstring, bytes + 1);
@@ -94,8 +94,8 @@ function invokeInsight(query_json_string: string | object): null | string | obje
     var result_json = insight_server.UTF8ToString(result_json_cstring);
     insight_server._free(cstring);
     insight_server._free(result_json_cstring);
-	insight_server.checkUnflushedContent();
-	
+    insight_server.checkUnflushedContent();
+    
     return JSON.parse(result_json);
 }
 
@@ -111,59 +111,59 @@ let hasWorkspaceFolderCapability: boolean = false;
 let hasDiagnosticRelatedInformationCapability: boolean = false;
 
 connection.onInitialize((params: InitializeParams) => {
-	let capabilities = params.capabilities;
+    let capabilities = params.capabilities;
 
-	// Does the client support the `workspace/configuration` request?
-	// If not, we fall back using global settings.
-	hasConfigurationCapability = !!(
-		capabilities.workspace && !!capabilities.workspace.configuration
-	);
-	hasWorkspaceFolderCapability = !!(
-		capabilities.workspace && !!capabilities.workspace.workspaceFolders
-	);
-	hasDiagnosticRelatedInformationCapability = !!(
-		capabilities.textDocument &&
-		capabilities.textDocument.publishDiagnostics &&
-		capabilities.textDocument.publishDiagnostics.relatedInformation
-	);
+    // Does the client support the `workspace/configuration` request?
+    // If not, we fall back using global settings.
+    hasConfigurationCapability = !!(
+        capabilities.workspace && !!capabilities.workspace.configuration
+    );
+    hasWorkspaceFolderCapability = !!(
+        capabilities.workspace && !!capabilities.workspace.workspaceFolders
+    );
+    hasDiagnosticRelatedInformationCapability = !!(
+        capabilities.textDocument &&
+        capabilities.textDocument.publishDiagnostics &&
+        capabilities.textDocument.publishDiagnostics.relatedInformation
+    );
 
-	const result: InitializeResult = {
-		capabilities: {
-			textDocumentSync: TextDocumentSyncKind.Incremental,
-			// Tell the client that this server supports code completion.
-			completionProvider: {
-				resolveProvider: true
-			},
-			hoverProvider: true
-		}
-	};
+    const result: InitializeResult = {
+        capabilities: {
+            textDocumentSync: TextDocumentSyncKind.Incremental,
+            // Tell the client that this server supports code completion.
+            completionProvider: {
+                resolveProvider: true
+            },
+            hoverProvider: true
+        }
+    };
 
-	if (hasWorkspaceFolderCapability) {
-		result.capabilities.workspace = {
-			workspaceFolders: {
-				supported: true
-			}
-		};
-	}
-	return result;
+    if (hasWorkspaceFolderCapability) {
+        result.capabilities.workspace = {
+            workspaceFolders: {
+                supported: true
+            }
+        };
+    }
+    return result;
 });
 
 connection.onInitialized(() => {
-	if (hasConfigurationCapability) {
-		// Register for all configuration changes.
-		connection.client.register(DidChangeConfigurationNotification.type, undefined);
-	}
-	if (hasWorkspaceFolderCapability) {
-		connection.workspace.onDidChangeWorkspaceFolders(_event => {
-			connection.console.log('Workspace folder change event received.');
-		});
-	}
+    if (hasConfigurationCapability) {
+        // Register for all configuration changes.
+        connection.client.register(DidChangeConfigurationNotification.type, undefined);
+    }
+    if (hasWorkspaceFolderCapability) {
+        connection.workspace.onDidChangeWorkspaceFolders(_event => {
+            connection.console.log('Workspace folder change event received.');
+        });
+    }
 });
 
 // The example settings
 interface ExampleSettings {
-	maxNumberOfProblems: number;
-	root: string;
+    maxNumberOfProblems: number;
+    root: string;
 }
 
 // The global settings, used when the `workspace/configuration` request is not supported by the client.
@@ -176,189 +176,189 @@ let globalSettings: ExampleSettings = defaultSettings;
 let documentSettings: Map<string, Thenable<ExampleSettings>> = new Map();
 
 connection.onDidChangeConfiguration(change => {
-	if (hasConfigurationCapability) {
-		// Reset all cached document settings
-		documentSettings.clear();
-	} else {
-		globalSettings = <ExampleSettings>(
-			(change.settings.adeptLanguageInsight || defaultSettings)
-		);
-	}
+    if (hasConfigurationCapability) {
+        // Reset all cached document settings
+        documentSettings.clear();
+    } else {
+        globalSettings = <ExampleSettings>(
+            (change.settings.adeptLanguageInsight || defaultSettings)
+        );
+    }
 
-	// Revalidate all open text documents
-	documents.all().forEach(validateTextDocument);
+    // Revalidate all open text documents
+    documents.all().forEach(validateTextDocument);
 });
 
 function getDocumentSettings(resource: string): Thenable<ExampleSettings> {
-	if (!hasConfigurationCapability) {
-		return Promise.resolve(globalSettings);
-	}
-	let result = documentSettings.get(resource);
-	if (!result) {
-		result = connection.workspace.getConfiguration({
-			scopeUri: resource,
-			section: 'adeptLanguageInsight'
-		});
-		documentSettings.set(resource, result);
-	}
-	return result;
+    if (!hasConfigurationCapability) {
+        return Promise.resolve(globalSettings);
+    }
+    let result = documentSettings.get(resource);
+    if (!result) {
+        result = connection.workspace.getConfiguration({
+            scopeUri: resource,
+            section: 'adeptLanguageInsight'
+        });
+        documentSettings.set(resource, result);
+    }
+    return result;
 }
 
 // Only keep settings for open documents
 documents.onDidClose(e => {
-	documentSettings.delete(e.document.uri);
+    documentSettings.delete(e.document.uri);
 });
 
 // The content of a text document has changed. This event is emitted
 // when the text document first opened or when its content has changed.
 documents.onDidChangeContent(change => {
-	validateTextDocument(change.document);
+    validateTextDocument(change.document);
 });
 
 async function validateTextDocument(textDocument: TextDocument): Promise<void> {
-	// In this simple example we get the settings for every validate run.
-	let settings = await getDocumentSettings(textDocument.uri);
-	let diagnostics: Diagnostic[] = [];
-	
-	var uri2path = require('file-uri-to-path');
-	var path = require('path');
-	var filename: string;
-	
-	try {
-		filename = path.resolve(uri2path(textDocument.uri));
-	} catch(err){
-		return;
-	}
+    // In this simple example we get the settings for every validate run.
+    let settings = await getDocumentSettings(textDocument.uri);
+    let diagnostics: Diagnostic[] = [];
+    
+    var uri2path = require('file-uri-to-path');
+    var path = require('path');
+    var filename: string;
+    
+    try {
+        filename = path.resolve(uri2path(textDocument.uri));
+    } catch(err){
+        return;
+    }
 
-	var response: null | string | any = invokeInsight({
-		"query": "ast",
-		"infrastructure": settings.root,
-		"filename": filename,
-		"code": textDocument.getText()
-	});
+    var response: null | string | any = invokeInsight({
+        "query": "ast",
+        "infrastructure": settings.root,
+        "filename": filename,
+        "code": textDocument.getText()
+    });
 
-	if(settings.root == ""){
-		connection.sendNotification("adeptLanguageInsight/noRoot");
-		return;
-	}
-	
-	if(response == null){
-		// Do nothing if WASM module isn't operation yet
-		// We will make sure to do a pass once it becomes so
-	} else if(typeof response == "string"){
-		let diagnostic: Diagnostic = {
-			severity: DiagnosticSeverity.Error,
-			range: {
-				start: textDocument.positionAt(0),
-				end: textDocument.positionAt(0)
-			},
-			message: response as string
-		};
-		
-		diagnostics.push(diagnostic);
-	} else if(Array.isArray(response.validation)){
-		(response.validation as any[]).forEach(element => {
-			var is_self = element.source.object === filename;
-			if(!is_self) return;
-			
-			let diagnostic: Diagnostic = {
-				severity: element.kind == "error" ? DiagnosticSeverity.Error : DiagnosticSeverity.Warning,
-				range: {
-					start: textDocument.positionAt(element.source.index),
-					end: textDocument.positionAt(Math.floor(element.source.index + element.source.stride))
-				},
-				message: element.message,
-				source: undefined
-			};
-			
-			diagnostics.push(diagnostic);
-		});
-	}
+    if(settings.root == ""){
+        connection.sendNotification("adeptLanguageInsight/noRoot");
+        return;
+    }
+    
+    if(response == null){
+        // Do nothing if WASM module isn't operation yet
+        // We will make sure to do a pass once it becomes so
+    } else if(typeof response == "string"){
+        let diagnostic: Diagnostic = {
+            severity: DiagnosticSeverity.Error,
+            range: {
+                start: textDocument.positionAt(0),
+                end: textDocument.positionAt(0)
+            },
+            message: response as string
+        };
+        
+        diagnostics.push(diagnostic);
+    } else if(Array.isArray(response.validation)){
+        (response.validation as any[]).forEach(element => {
+            var is_self = element.source.object === filename;
+            if(!is_self) return;
+            
+            let diagnostic: Diagnostic = {
+                severity: element.kind == "error" ? DiagnosticSeverity.Error : DiagnosticSeverity.Warning,
+                range: {
+                    start: textDocument.positionAt(element.source.index),
+                    end: textDocument.positionAt(Math.floor(element.source.index + element.source.stride))
+                },
+                message: element.message,
+                source: undefined
+            };
+            
+            diagnostics.push(diagnostic);
+        });
+    }
 
-	if(response && response.identifierTokens){
-		identifierTokens = response.identifierTokens;
-	}
+    if(response && response.identifierTokens){
+        identifierTokens = response.identifierTokens;
+    }
 
-	if(response && response.ast){
-		ast = response.ast;
-		autoCompletion = constructAutoCompletions();
-	}
+    if(response && response.ast){
+        ast = response.ast;
+        autoCompletion = constructAutoCompletions();
+    }
 
-	// Send the computed diagnostics to VSCode.
-	connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
+    // Send the computed diagnostics to VSCode.
+    connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
 }
 
 connection.onDidChangeWatchedFiles(_change => {
-	// Monitored files have change in VSCode
-	connection.console.log('We received an file change event');
+    // Monitored files have change in VSCode
+    connection.console.log('We received an file change event');
 });
 
 // This handler provides the initial list of the completion items.
 connection.onCompletion(
-	(_textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
-		// The pass parameter contains the position of the text document in
-		// which code complete got requested. For the example we ignore this
-		// info and always provide the same completion items.
-		return autoCompletion.completionItems;
-	}
+    (_textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
+        // The pass parameter contains the position of the text document in
+        // which code complete got requested. For the example we ignore this
+        // info and always provide the same completion items.
+        return autoCompletion.completionItems;
+    }
 );
 
 function constructAutoCompletions(): AutoCompletions {
-	var completions: AutoCompletions = new AutoCompletions();
-	var symbols: any[] = [];
+    var completions: AutoCompletions = new AutoCompletions();
+    var symbols: any[] = [];
 
-	if(ast) {
-		ast.composites.forEach((f: any) => {
-			f._completionItemKind = CompletionItemKind.Struct
-		});
-		symbols = symbols.concat(ast.composites);
+    if(ast) {
+        ast.composites.forEach((f: any) => {
+            f._completionItemKind = CompletionItemKind.Struct
+        });
+        symbols = symbols.concat(ast.composites);
 
-		ast.enums.forEach((f: any) => {
-			f._completionItemKind = CompletionItemKind.Enum
-		});
-		symbols = symbols.concat(ast.enums);
+        ast.enums.forEach((f: any) => {
+            f._completionItemKind = CompletionItemKind.Enum
+        });
+        symbols = symbols.concat(ast.enums);
 
-		ast.functions.forEach((f: any) => {
-			f._completionItemKind = CompletionItemKind.Function
-		});
-		symbols = symbols.concat(ast.functions);
-	}
+        ast.functions.forEach((f: any) => {
+            f._completionItemKind = CompletionItemKind.Function
+        });
+        symbols = symbols.concat(ast.functions);
+    }
 
-	symbols.forEach((symbol) => {
-		var item: CompletionItem = {label: symbol.name, kind: symbol._completionItemKind};
-		var details: CompletionDetails = new CompletionDetails(symbol.definition ? symbol.definition : "", symbol.documentation ? symbol.documentation : "");
-		
-		completions.add(item, details);
-	});
+    symbols.forEach((symbol) => {
+        var item: CompletionItem = {label: symbol.name, kind: symbol._completionItemKind};
+        var details: CompletionDetails = new CompletionDetails(symbol.definition ? symbol.definition : "", symbol.documentation ? symbol.documentation : "");
+        
+        completions.add(item, details);
+    });
 
-	return completions;
+    return completions;
 }
 
 // This handler resolves additional information for the item selected in
 // the completion list.
 connection.onCompletionResolve(
-	(item: CompletionItem): CompletionItem => {
-		return autoCompletion.getDetailedCompletionItem(item);
-	}
+    (item: CompletionItem): CompletionItem => {
+        return autoCompletion.getDetailedCompletionItem(item);
+    }
 );
 
 connection.onHover((params: HoverParams): Hover | null => {
-	if(!ast || !identifierTokens) return null;
+    if(!ast || !identifierTokens) return null;
 
-	for(var identifier of identifierTokens){  
-		if(params.position.line != identifier.range.start.line) continue;
-		if(params.position.line != identifier.range.end.line) continue;
-		if(params.position.character < identifier.range.start.character) continue;
-		if(params.position.character > identifier.range.end.character) continue;
+    for(var identifier of identifierTokens){  
+        if(params.position.line != identifier.range.start.line) continue;
+        if(params.position.line != identifier.range.end.line) continue;
+        if(params.position.character < identifier.range.start.character) continue;
+        if(params.position.character > identifier.range.end.character) continue;
 
-		var definitions: string[] = ast.functions.filter((f: any) => f.name == identifier.content).map((f: any) => f.definition);
-		return definitions.length == 0 ? null : {
-			contents: {kind: "plaintext", value: definitions.join("\n")},
-			range: identifier.range
-		};
-	};
-	
-	return null;
+        var definitions: string[] = ast.functions.filter((f: any) => f.name == identifier.content).map((f: any) => f.definition);
+        return definitions.length == 0 ? null : {
+            contents: {kind: "plaintext", value: definitions.join("\n")},
+            range: identifier.range
+        };
+    };
+    
+    return null;
 });
 
 // Make the text document manager listen on the connection
