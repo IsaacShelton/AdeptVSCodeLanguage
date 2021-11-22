239fe10fd37b25fd34af1a04e64dd47d32b6561c
IsaacShelton <isaac.a.shelton@gmail.com>
2021-11-21 22:34:56 -0600

    Added support for records

--- server/out/insight_server.js
+++ server/out/insight_server.js
@@ -1233,10 +1233,10 @@ function updateGlobalBufferAndViews(buf) {
   Module['HEAPF64'] = HEAPF64 = new Float64Array(buf);
 }
 
-var STACK_BASE = 5275472,
+var STACK_BASE = 5275920,
     STACKTOP = STACK_BASE,
-    STACK_MAX = 32592,
-    DYNAMIC_BASE = 5275472;
+    STACK_MAX = 33040,
+    DYNAMIC_BASE = 5275920;
 
 assert(STACK_BASE % 16 === 0, 'stack must start aligned');
 assert(DYNAMIC_BASE % 16 === 0, 'heap must start aligned');
